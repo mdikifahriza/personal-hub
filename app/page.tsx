1ffd74fc15dababb3f1a809f886bbefa028aef47
mdikifahriza <147649626+mdikifahriza@users.noreply.github.com>
2026-01-20 19:30:58 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function LoginPage() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
               </svg>
             </div>
-            <h1 className="text-xl font-semibold text-gray-900">Catatan Harian</h1>
+            <h1 className="text-xl font-semibold text-gray-900">Catatan Pengemis</h1>
             <p className="text-sm text-gray-600 mt-1">Masukkan password</p>
           </div>
           
